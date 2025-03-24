90a9a58f9ed41aabf49b344cfc7aa65f14cc9692
aprillmezu <161106154+aprillmezu@users.noreply.github.com>
2025-03-24 09:25:35 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# fasum_test
+
+A new Flutter project.
---
+++ lib/screens/home_screen.dart
@@ -0,0 +1,25 @@
+import 'package:flutter/material.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:fasum_test/screens/sign_in_screen.dart';
+
+class HomeScreen extends StatelessWidget {
+  const HomeScreen({super.key});
+
+  Future<void> signOut(BuildContext context) async {
+    await FirebaseAuth.instance.signOut();
+    Navigator.of(
+      context,
+    ).pushReplacement(MaterialPageRoute(builder: (context) => SignInScreen()));
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Home"),
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        actions: [Icon],
+      ),
+    );
+  }
+}
