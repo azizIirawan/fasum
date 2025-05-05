ca92c6c6189612652a40d94d7e4c7cb93f6fc9cc
azizIirawan <azizirawan_2226250114@mhs.mdp.ac.id>
2025-05-05 09:30:02 +0700

    P20

--- lib/main.dart
+++ lib/main.dart
@@ -1,15 +1,13 @@
-import 'package:fasum/firebase_options.dart';
-import 'package:fasum/screens/home_screen.dart';
-import 'package:fasum/screens/sign_in_screen.dart';
+import 'package:fasum_test/firebase_options.dart';
+import 'package:fasum_test/screens/home_screen.dart';
+import 'package:fasum_test/screens/sign_in_screen.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
